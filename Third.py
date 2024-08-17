849eb668bea0a923a24f5718c49e4cba303df888
Ryan Moore <125109206+RyanCrf7@users.noreply.github.com>
2024-08-16 23:32:40 -0300

    Update Third.py

--- Third.py
+++ Third.py
@@ -2,6 +2,6 @@ num1 = int(input("Enter the value of the first number: "))
 num2 = int(input("Enter the value of the second number: "))
 num3 = int(input("Enter the value of the third number: "))
 average = (num1 + num2 + num3) / 3
-print(f"The average of the numbers: {num1}, {num2}, {num3} is {average}")
+print(f" The average of the numbers: {num1}, {num2}, {num3} is {average:.2f}")
 
 exit()
